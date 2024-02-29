4ffcdfbab821f929a865bc4817e87000bfef8fff
Karolina Nowak <nowakina15@gmail.com>
2024-02-29 16:44:51 +0100

    dodano pliki z zajec lab0

---
+++ LAB1/LAB1/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class FizzBuzz
+    {
+        int upperBound = 10;
+
+        public FizzBuzz(int arg)
+        {
+            upperBound = arg;
+        }
+
+        public void Display()
+        {
+            for (int i = 1; i <= upperBound; i++)
+            {
+                if(i % 5==0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if (i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void SetUpperBound(int bound)
+        {
+            upperBound = bound;
+        }
+    }
+}
---
+++ LAB1/LAB1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LAB1;
+
+FizzBuzz fz1 = new FizzBuzz(15);
+//fz1.SetUpperBound(20);
+fz1.Display();
